b4260d37883215226350e82a87997ac01960074b
twotry9 <69636036+twotry9@users.noreply.github.com>
2025-06-30 13:22:05 +0000

    Update 1.26.cpp

--- 1.26.cpp
+++ 1.26.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main() {
     double radius;
 
-    // Ввод радиуса
     cout << "Введите радиус окружности: ";
     cin >> radius;
 
-    // Вычисление диаметра (D = 2 * R)
     double diameter = 2 * radius;
 
-    // Вывод результата
     cout << "Диаметр окружности: " << diameter << endl;
 
     return 0;
