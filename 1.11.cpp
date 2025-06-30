096740094527e18565fd8f1ee0a1ecc755707c4b
twotry9 <69636036+twotry9@users.noreply.github.com>
2025-06-30 12:56:27 +0000

    Add files via upload

---
+++ 1.11.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2, num3, num4;
+
+    cout << "Введите числa: ";
+    cin >> num1 >> num2 >> num3 >> num4;
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num3 << endl;
+    cout << num4 << endl;
+
+    return 0;
+}
